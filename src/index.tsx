97ce6c5c3acfd88eed276695f58b8068b3adb186
Juan Ospina <juan.ospina@unp.gov.co>
2024-10-16 08:39:09 -0500

    fix(Toastify):implementacion servicio toastify)

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AutenticacionProvider } from "./providers/AutenticacionProvider";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container!);
 
@@ -10,5 +13,6 @@ root.render(
   // El componente AutenticacionProvider envuelve al componente Login para validar token de autenticacion
   <AutenticacionProvider>
     <App />
+    <ToastContainer />
   </AutenticacionProvider>
 );
