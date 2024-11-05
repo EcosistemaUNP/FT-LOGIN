a2e827410733c80923aba2937f76d1fe0dbaffe7
Juan Ospina <juan.ospina@unp.gov.co>
2024-11-05 11:57:50 -0500

    feat(registro):Añadido la pagina de registro de usuarios externos

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import IniciarSesion from "./InicioSesion";
+import FormularioPage from "./pages/FormularioPage";
+import RegistroPage from "./pages/RegistroPage";
 import "./styles/App.css";
 
 const App: React.FC = () => {
   return (
-    <div className="main-container">
-      <IniciarSesion />
-    </div>
+    <FormularioPage />
+    // <div className="main-container">
+    // </div>
   );
 };
 
